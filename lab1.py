20017382f69abb103d2117db2f74772324932fcc
jmailnaif <jamailotb15@gmail.com>
2024-02-08 22:23:44 +0300

    loop

---
+++ lab1.py
@@ -0,0 +1,5 @@
+for n in range(45,210):
+    if n ==100:
+        continue
+    print(n)
+    
---
+++ lab2.py
@@ -0,0 +1,6 @@
+answal = input("what is the product of 7*24?")
+   print(answal)
+    if answal == (7*24):
+         print("you answered this questoncorrectly")
+    else:
+        print("your answeris wrong try aaganin")
