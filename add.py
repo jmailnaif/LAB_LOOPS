662ffdf9e95678410aabc6eb24950d9d4b85aef2
jmail <jamailotb15@gmail.com>
2024-02-17 10:44:29 +0300

    add

---
+++ add.py
@@ -0,0 +1,8 @@
+#2
+asl_1:str = input("what is the product of 7 * 24 ? ")
+if asl_1== "168":
+     print("you answered this question correctly")
+     break
+else:
+    print("your answer is wrong Try again")
+    
---
+++ edd.py
@@ -0,0 +1,7 @@
+for num in range(45,211):
+  if num == 100:
+    continue
+  print(num)
+  if num == 205:
+    break
+print(num)
